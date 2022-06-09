8987f2b816aef9d6bf89227161c4255d36299e76
RITIK <ritikraj6120@gmail.com>
2022-06-09 06:45:34 +0530

    trying second attempt to resolve cors

--- index.js
+++ index.js
@@ -6,9 +6,16 @@ require('dotenv').config()
 connectToMongo();
 const app = express()
 var port = process.env.PORT || 5000
-app.use(cors({
-	origin: 'http://localhost:3000/'
-}))
+// app.use(cors({
+// 	origin: 'http://localhost:3000/'
+// }))
+app.use(function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+	res.setHeader('Access-Control-Allow-Headers', '*');
+	res.setHeader('Access-Control-Allow-Credentials', true);
+	next();
+});
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "client", "build")))
 // Available Routes
