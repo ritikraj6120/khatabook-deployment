8e238b55d63eafc0dd8508e364ee47820dc3327f
RITIK <ritikraj6120@gmail.com>
2022-06-09 06:26:51 +0530

    attempting to resolve cors error

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ require('dotenv').config()
 connectToMongo();
 const app = express()
 var port = process.env.PORT || 5000
-app.use(cors())
+app.use(cors({
+	origin: 'http://localhost:3000/'
+}))
 app.use(express.json())
 app.use(express.static(path.join(__dirname, "client", "build")))
 // Available Routes
